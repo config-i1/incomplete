4502ae20af959df540ae262a30f8a02fe9610bee
Ivan Svetunkov <ivan@svetunkov.ru>
2021-12-25 09:42:59 +0000

    Improved plot

--- R/civilization.R
+++ R/civilization.R
@@ -5,4 +5,4 @@ plot(0,0,col="white",xaxt="n",yaxt="n",bty="n",xlab="",ylab="",
 for(i in 0:720){
     abline(a=0,b=tan(i/2),col=rgb(0.2,0.2,0.3,1))
 }
-points(0,0,col="white",pch=13)
+points(0.2,0.3,col="white",pch=16)
